af2c9c639b1050011b9408429bcccf8356366a74
KmEnriquez <kentmathewenriquez@gmail.com>
2023-07-13 09:44:03 +0800

    updated

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,12 @@
 const toggle = document.querySelector(".toggle__menu");
 const headNav = document.querySelector(".header__nav");
+const body = document.querySelector(".body");
+const dim = document.querySelector(".dim");
 
 toggle.addEventListener("click", ()=> {
     headNav.classList.toggle("open");
     // console.log("click")
     toggle.classList.toggle("open");
+    body.classList.toggle("disable");
+    dim.classList.toggle("show");
 });
